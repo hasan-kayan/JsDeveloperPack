c4fa032c7c8bd57e64d51591727adef3d74c535f
hasan-kayan <hasankayan2000@hotmail.com>
2024-07-02 11:07:39 +0300

    Additional Styles

--- src/logger/logStyles.js
+++ src/logger/logStyles.js
@@ -5,7 +5,10 @@ const styles = {
     warning: 'color: orange; font-weight: bold;',
     error: 'color: red; font-weight: bold;',
     success: 'color: green; font-weight: bold;',
-    debug: 'color: purple; font-weight: bold;'
+    debug: 'color: purple; font-weight: bold;',
+    highlight: 'background-color: yellow; color: black; font-weight: bold;',
+    emphasize: 'font-style: italic; color: gray;',
+    important: 'color: red; font-size: 1.2em; font-weight: bold;'
 };
 
 module.exports = styles;
